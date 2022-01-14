ee15722264d9048ba2013a4a381d3f9091450732
Ernest <muiboonyang@gmail.com>
2022-01-14 12:09:29 +0800

    Update NavBar.js

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -7,38 +7,36 @@ const NavBar = () => {
   return (
     <>
       <Navbar bg="dark" variant="dark">
-        <Container>
-          <Navbar.Brand href="/">
-            <img
-              alt=""
-              src="https://i.imgur.com/kNVaDX5.png"
-              width="30"
-              height="30"
-              className="d-inline-block"
-            />{" "}
-            &nbsp; <b>My Show App</b>
-          </Navbar.Brand>
+        <Navbar.Brand href="/">
+          <img
+            alt=""
+            src="https://i.imgur.com/kNVaDX5.png"
+            width="30"
+            height="30"
+            className="d-inline-block"
+          />{" "}
+          &nbsp; <b>My Show App</b>
+        </Navbar.Brand>
 
-          <Nav className="me-auto">
-            <a href="/">
-              <i className="fa fa-fw fa-home"></i> Home
-            </a>
+        <Nav className="me-auto">
+          <a href="/">
+            <i className="fa fa-fw fa-home"></i> Home
+          </a>
 
-            <a href="/search">
-              <i className="fa fa-fw fa-search"></i> Search
-            </a>
+          <a href="/search">
+            <i className="fa fa-fw fa-search"></i> Search
+          </a>
 
-            <a href="/contact">
-              <i className="fa fa-fw fa-envelope"></i> Contact
-            </a>
-          </Nav>
+          <a href="/contact">
+            <i className="fa fa-fw fa-envelope"></i> Contact
+          </a>
+        </Nav>
 
-          <Nav placement="end">
-            <a href="/login">
-              <i className="fa fa-fw fa-user"></i> Log In
-            </a>
-          </Nav>
-        </Container>
+        <Nav placement="end">
+          <a href="/login">
+            <i className="fa fa-fw fa-user"></i> Log In
+          </a>
+        </Nav>
       </Navbar>
     </>
   );
